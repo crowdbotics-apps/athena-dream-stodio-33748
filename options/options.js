5a531d1fe425307552ff4c8b8c292f0ded1ceec2
Crowdbotics <team@crowdbotics.com>
2022-02-20 18:48:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "athena_dream_stodio_33748",
+  url: "https://athena_dream_stodio_33748.botics.co",
+  api: "https://athena_dream_stodio_33748.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
