96c51f0550c2f71ec629d0124361809ced9fdb21
Crowdbotics <team@crowdbotics.com>
2022-02-20 18:49:35 +0000

    Update admin title

--- backend/athena_dream_stodio_33748/urls.py
+++ backend/athena_dream_stodio_33748/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Athena Dream Stodio"
-admin.site.site_title = "Athena Dream Stodio Admin Portal"
-admin.site.index_title = "Athena Dream Stodio Admin"
+admin.site.site_header = "Athena Dream Studio"
+admin.site.site_title = "Athena Dream Studio Admin Portal"
+admin.site.index_title = "Athena Dream Studio Admin"
 
 # swagger
 api_info = openapi.Info(
